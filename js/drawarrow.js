f4128eefb286f74bbe873789ac236002158ac3f9
raytracer <swchris90@googlemail.com>
2015-01-20 15:22:27 +0100

    added a drawCurve function

--- js/drawarrow.js
+++ js/drawarrow.js
@@ -9,3 +9,34 @@ var drawArrow = function(sketch, cx, cy, len, angle){
 	sketch.line(0, 0, -8, 8);
 	sketch.pop();
 };
+
+var drawCurve = function(sketch, x1, x2, x3, x4, y1, y2, y3, y4, numArrows) {
+    var warm = sketch.color(255, 0, 0);
+    var cool = sketch.color(0, 0, 255);
+
+    var startx = x1;
+    var starty = x2;
+    var start_interx = x3;
+    var start_intery = x4;
+
+    var endx = y1;
+    var endy = y2;
+    var end_interx = y3;
+    var end_intery = y4;
+
+    var t_base =  (sketch.frameCount/100.0)%1;
+    //NO equal spacing, solution not trivial
+    //http://imaginary-institute.com/resources/Tech%20Note%2008%20Equal%20Spacing%20Along%20Curves.pdf
+		for (var i = 0; i < numArrows; i++) {
+			var t = (t_base + 1/(i + 1)) % 1;
+			sketch.stroke(sketch.lerpColor(cool, warm, t));
+			var x = sketch.bezierPoint(startx, starty, start_interx, start_intery, t);
+			var y = sketch.bezierPoint(endx, endy, end_interx, end_intery, t);
+
+			var ax = sketch.bezierTangent(startx, starty, start_interx, start_intery, t);
+			var ay = sketch.bezierTangent(endx, endy, end_interx, end_intery, t);
+
+			var a = sketch.atan2(ay, ax);
+			drawArrow(sketch,x,y,30,a);
+		}
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	startSection = new Section("ITC", "#intro-text", convectionSketch, quizdata);
+	new Section("ITC", "#intro-text", defaultSketch, quizdata);
 	startSection.start();
 });
 
@@ -109,52 +110,8 @@ var defaultSketch = function(sketch) {
   sketch.draw = function () {
     sketch.background(255);
     sketch.noFill();
-
-    var warm = sketch.color(255, 0, 0);
-    var cool = sketch.color(0, 0, 255);
-
-    var startx = 100;
-    var starty = 200;
-    var start_interx = 200;
-    var start_intery = 300;
-
-    var endx = 0;
-    var endy = 10;
-    var end_interx = 400;
-    var end_intery = 500;
-
-    var numArrows = 2;
-    var numCurves = 10;
-    var offset = 20;
-
-    //curve that I want an object/sprite to move down
-    //bezier(800, 0,1000,10,900,450,700,682);
-
-    var t_base =  (sketch.frameCount/100.0)%1;
-
-    //NO equal spacing, solution not trivial
-    //http://imaginary-institute.com/resources/Tech%20Note%2008%20Equal%20Spacing%20Along%20Curves.pdf
-    for (var c = 0; c < numCurves; c++) {
-      //endx += c * offset;
-      //end_interx += c * offset;
-      startx += offset;
-      starty += offset;
-      start_interx += offset;
-      start_intery += offset;
-
-      for (var i = 0; i < numArrows; i++) {
-        var t = (t_base + 1/(i + 1)) % 1;
-        sketch.stroke(sketch.lerpColor(cool, warm, t));
-        var x = sketch.bezierPoint(startx, starty, start_interx, start_intery, t);
-        var y = sketch.bezierPoint(endx, endy, end_interx, end_intery, t);
-
-        var ax = sketch.bezierTangent(startx, starty, start_interx, start_intery, t);
-        var ay = sketch.bezierTangent(endx, endy, end_interx, end_intery, t);
-
-        var a = sketch.atan2(ay, ax);
-        drawArrow(sketch,x,y,30,a);
-      }
-    }
+		
+		drawCurve(sketch,0,0,50,100,0,0,0,400,3);
   };
 
 };
