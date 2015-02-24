7c1156a6a47231f14f159813682e2eec09b7044a
EinKaenguru <EinKaenguru@users.noreply.github.com>
2015-02-24 18:53:26 +0100

    SunAltitude position moved to top, date shows day

--- js/sunAltitude.js
+++ js/sunAltitude.js
@@ -7,13 +7,16 @@ var sunSketch = function(sketch) {
 		sketch.createCanvas(sketch.w/2.0, sketch.h);
 		
 		sketch.earthsize = Math.min(sketch.w/2,sketch.h)*0.7;
+		sketch.earthmiddle = sketch.earthsize/2;
 		
+		//Slider
 		sketch.slider = sketch.createSlider(0,364,0);
-		sketch.slider.position(sketch.w/4-sketch.earthsize/2, sketch.earthsize/2+sketch.h/2+10);
+		sketch.slider.position(sketch.w/4-sketch.earthsize/2, sketch.earthsize+10);
 		sketch.slider.size(sketch.earthsize, 10);
 		
-		sketch.earth = sketch.loadImage("images/globe.png");
 		
+		sketch.earth = sketch.loadImage("images/globe.png");
+				
 		sketch.smooth();
 	
 		
@@ -24,13 +27,12 @@ var sunSketch = function(sketch) {
 		sketch.stroke(0);
 		sketch.fill(255);
 		
+		//Erdkugel
 		sketch.imageMode(sketch.CENTER);
-		sketch.image(sketch.earth,sketch.w/4,sketch.h/2,sketch.earthsize,sketch.earthsize);
+		sketch.image(sketch.earth,sketch.w/4,sketch.earthmiddle,sketch.earthsize,sketch.earthsize);
 		
-		
-		sketch.line(sketch.w/4-sketch.earthsize/2,sketch.h/2,sketch.w/4+sketch.earthsize/2,sketch.h/2)
-		
-		sketch.stroke(255);
+		//Aequator
+		sketch.line(sketch.w/4-sketch.earthsize/2,sketch.earthmiddle,sketch.w/4+sketch.earthsize/2,sketch.earthmiddle)
 		
 		sketch.sun();
 		
@@ -50,33 +52,42 @@ var sunSketch = function(sketch) {
 		day}			
 	
 		var scale = 0.2;
-		var height = sketch.h/2+(pos-91)/182*sketch.earthsize*scale;
+		var height = sketch.earthmiddle+(pos-91)/182*sketch.earthsize*scale;
 		
+		//Wendekreise
+		sketch.stroke(255);
 		sketch.strokeWeight(0.5);
-		sketch.line(sketch.w/4-sketch.earthsize/2,sketch.h/2+sketch.earthsize*scale/2,sketch.w/4+sketch.earthsize/2,sketch.h/2+sketch.earthsize*scale/2);
-		sketch.line(sketch.w/4-sketch.earthsize/2,sketch.h/2-sketch.earthsize*scale/2,sketch.w/4+sketch.earthsize/2,sketch.h/2-sketch.earthsize*scale/2);
+		sketch.line(sketch.w/4-sketch.earthsize/2,sketch.earthmiddle+sketch.earthsize*scale/2,sketch.w/4+sketch.earthsize/2,sketch.earthmiddle+sketch.earthsize*scale/2);
+		sketch.line(sketch.w/4-sketch.earthsize/2,sketch.earthmiddle-sketch.earthsize*scale/2,sketch.w/4+sketch.earthsize/2,sketch.earthmiddle-sketch.earthsize*scale/2);
 		
+		//Sonne
 		sketch.strokeWeight(1);
 		sketch.stroke(0);
 		sketch.fill(255,255,0);
 		sketch.ellipse(sketch.w/4,height,sketch.earthsize*0.1,sketch.earthsize*0.1);
-		
-		
-		
-	
+			
 	
 	}
 	
 	sketch.drawDate = function() {
-		var day = sketch.slider.value();
+		var day = sketch.slider.value()+1;
+		var month = 0;
 		var months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
-
-		var month = months[Math.floor(day/365 * 12)];
+		var days = [31,59,90,120,151,181,212,243,273,304,334,365];
+	
+			for (i = 11; i >= 0; i--) {
+				if(day > days[i]){
+					day = day-days[i];
+					month = i+1;
+				} 			
+			}		
+		
+		var date = day+'. '+months[month];
 		sketch.stroke(0);
 		sketch.fill(0);
 		var textSize = 15;
 		sketch.textFont('Helvetica');
 		sketch.textSize(textSize);
-		sketch.text(month, sketch.w/4 - textSize * (month.length/4), sketch.h - textSize);
+		sketch.text(date, sketch.w/4 - textSize * 3, sketch.earthsize+40);
 	}
 }
